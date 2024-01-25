08dd8c188dbbc08432867dca4c49a281c5cf4c87
J222 <baram789456@gmail.com>
2024-01-25 20:15:16 +0100

    [Refactor] Add motherboard vendor name table

--- src/services/system/format/motherboardFormatService.ts
+++ src/services/system/format/motherboardFormatService.ts
@@ -1,5 +1,17 @@
 import { ISystemInfo } from '@/types/dto/commonDto.ts';
 import { IMotherboard } from '@/types/model/computer/motherboardType.ts';
+import { MOTHERBOARD_SUB_VENDOR_NAME_TABLE } from '@/constants/motherboardConstants.ts';
+
+export function formatMotherboardVendor(sourceName: string): string {
+  if (!sourceName) return 'UNKNOWN';
+
+  const vendor =
+    MOTHERBOARD_SUB_VENDOR_NAME_TABLE.find((vendor) =>
+      sourceName.toLowerCase().includes(vendor.toLowerCase()),
+    ) ?? sourceName;
+
+  return vendor;
+}
 
 export function transformMotherboard(
   dto: ISystemInfo,
@@ -11,7 +23,9 @@ export function transformMotherboard(
         dto.system.motherboard[0].Product ??
         dto.system.motherboard[0].Model ??
         dto.system.motherboard[0].Caption, // Caption or Name
-      vendorName: dto.system.motherboard[0].Manufacturer,
+      vendorName: formatMotherboardVendor(
+        dto.system.motherboard[0].Manufacturer,
+      ),
       chipset: dto.system.motherboard[0].Product,
     };
   }
