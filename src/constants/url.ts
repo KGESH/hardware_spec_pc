76094a0f22c72ff57e23f831be371ac71365ac79
J222 <baram789456@gmail.com>
2024-01-28 00:44:51 +0100

    [Build] Windows test

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,5 @@
-export const ESTIMATE_HOME_PAGE_URL = 'http://localhost:3000';
+export const ESTIMATE_HOME_PAGE_URL =
+  ' https://1f11-103-249-31-27.ngrok-free.app';
 
-export const ESTIMATE_API_BASE_URL = 'http://localhost:3333';
+export const ESTIMATE_API_BASE_URL =
+  'https://ec99-103-249-31-27.ngrok-free.app';
