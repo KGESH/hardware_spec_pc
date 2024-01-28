5ba8400232d0eb92a35097efc4f9b8587a635dab
J222 <baram789456@gmail.com>
2024-01-28 01:05:51 +0100

    [Build] Windows test

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,6 +8,15 @@ pub mod system;
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![event::get_system_info])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
