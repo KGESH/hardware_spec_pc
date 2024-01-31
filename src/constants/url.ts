ebd14015575dea100cfa14bc2102f80ea0b03de7
J222 <baram789456@gmail.com>
2024-02-01 00:17:58 +0100

    [Refactor] Add production url

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,4 +1,5 @@
 export const ESTIMATE_HOME_PAGE_URL =
-  'https://a108-141-8-97-141.ngrok-free.app';
+  'https://main.d2hpnyo1fl0365.amplifyapp.com';
 
-export const ESTIMATE_API_BASE_URL = 'https://505d-141-8-97-141.ngrok-free.app';
+export const ESTIMATE_API_BASE_URL =
+  'https://zcwceymcsf.ap-northeast-1.awsapprunner.com';
